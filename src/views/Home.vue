59de3f4961ca90a61dd1db9ed0402f0c8b381f82
Afonso Barbosa <afonso33barbosa@gmail.com>
2022-02-01 10:26:07 -0400

    adicionado global.css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,11 @@
 <template>
-  <hello-world />
+  <p>teste</p>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'Home',
 
-    components: {
-      HelloWorld,
-    },
   }
 </script>
