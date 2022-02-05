580242db262f0f2353a98571953a0e64d1c502c3
Afonso Barbosa <afonso33barbosa@gmail.com>
2022-02-05 10:07:54 -0400

    adicionado overlay

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,246 +1,305 @@
 <template>
-  <div class="container">
+  <div style="height: 100%;">
 
-    <vue-particles
-      color="#0a0f1d"
-      :particleOpacity="0.28"
-      :particlesNumber="100"
-      shapeType="circle"
-      :particleSize="2"
-      linesColor="#0a0f1d"
-      :linesWidth="1"
-      :lineLinked="true"
-      :lineOpacity="0.1"
-      :linesDistance="220"
-      :moveSpeed="2"
-      :hoverEffect="true"
-      hoverMode="grab"
-      :clickEffect="true"
-      clickMode="push"
-      class="content-particles"
-    >
-    </vue-particles>
+    <div class="container">
+      <vue-particles
+        color="#0a0f1d"
+        :particleOpacity="0.28"
+        :particlesNumber="85"
+        shapeType="circle"
+        :particleSize="2"
+        linesColor="#0a0f1d"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.1"
+        :linesDistance="160"
+        :moveSpeed="2"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+        class="content-particles"
+      >
+      </vue-particles>
 
-    <div class="d-flex align-center container-top">
-      <v-img
-        alt="Linhas da parte de cima da pagina"
-        cover
-        src="img/home-cima.png"
-        class="faixa-top"
-      />
-    </div>
-    
-    <div class="d-flex center-page">
-      <div class="left-center-box d-flex align-center justify-center">
+      <div class="d-flex align-center container-top">
+        <v-img
+          alt="Linhas da parte de cima da pagina"
+          cover
+          src="img/home-cima.png"
+          class="faixa-top"
+        />
+      </div>
+      
+      <div class="d-flex center-page">
+        <div class="left-center-box d-flex align-center justify-center">
 
-        <div class="reduction d-flex justify-space-around align-center">
-          <div class="d-flex box-menu">
+          <div class="reduction d-flex justify-space-around align-center">
+            <div class="d-flex box-menu">
 
+              <v-hover
+                v-slot="{ hover }"
+              >
+                <div
+                  class="pl-2 pr-2 pt-2 pb-2 d-flex flex-column align-center justify-center box"
+                  :class="hover ? 'hover-box' : ''"
+                >
+                  <div
+                    class="circle d-flex align-center justify-center"
+                    :class="hover ? 'hover-circle' : ''"
+                    @click="teste_link()"
+                  >
+                    <v-icon
+                      size="40"
+                      class="icon"
+                      :class="hover ? 'hover-icon' : ''"
+                    >
+                      mdi-book-open-variant
+                    </v-icon>
+                  </div>
+
+                  <span
+                    class="text-circle"
+                    :class="hover ? 'text-circle-hover' : ''"
+                  >
+                    Sobre a empresa
+                  </span>
+                </div>
+              </v-hover>
+
+              <v-divider vertical></v-divider>
+
+              <v-hover
+                v-slot="{ hover }"
+              >
+                <div
+                  class="pl-2 pr-2 pt-2 pb-2 d-flex flex-column align-center justify-center box"
+                  :class="hover ? 'hover-box' : ''"
+                  @click="teste_link()"
+                >
+                  <div
+                    class="circle-orange d-flex align-center justify-center"
+                    :class="hover ? 'hover-circle' : ''"
+                  >
+                    <v-icon
+                      size="40"
+                      class="icon-orange"
+                      :class="hover ? 'hover-icon' : ''"
+                    >
+                      mdi-text-box-check-outline
+                    </v-icon>
+                  </div>
+
+                  <span
+                    class="text-circle"
+                    :class="hover ? 'text-circle-hover' : ''"
+                  >
+                    Conhecer os planos
+                  </span>
+                </div>
+              </v-hover>
+
+              <v-divider vertical></v-divider>
+
+              <v-hover
+                v-slot="{ hover }"
+              >
+                <div
+                  class="pl-2 pr-2 pt-2 pb-2 d-flex flex-column align-center justify-center box"
+                  :class="hover ? 'hover-box' : ''"
+                  @click="teste_link()"
+                >
+                  <div
+                    class="circle d-flex align-center justify-center mb-2"
+                    :class="hover ? 'hover-circle' : ''"
+                  >
+                    <v-icon
+                      size="40"
+                      class="icon"
+                      :class="hover ? 'hover-icon' : ''"
+                    >
+                      mdi-phone-message
+                    </v-icon>
+                  </div>
+
+                  <span
+                    class="text-circle text-center"
+                    :class="hover ? 'text-circle-hover' : ''"
+                  >
+                    Solicitar Contato
+                  </span>
+                </div>
+              </v-hover>
+
+              <v-divider vertical></v-divider>
+
+              <v-hover
+                v-slot="{ hover }"
+              >
+                <div
+                  class="pl-2 pr-2 pt-2 pb-2 d-flex flex-column align-center justify-center box destaque-box"
+                  :class="hover ? 'hover-box' : ''"
+                  @click="redirecionar_link('http://sgp.master.net.br:8000/accounts/central/login')"
+                >
+                  <div
+                    class="circle d-flex align-center justify-center mb-2"
+                    :class="hover ? 'destaque-hover-circle' : ''"
+                  >
+                    <v-icon
+                      size="40"
+                      class="icon"
+                      :class="hover ? 'hover-icon' : ''"
+                    >
+                      mdi-face-agent
+                    </v-icon>
+                  </div>
+
+                  <span
+                    class="text-circle text-center destaque-text-circle"
+                    :class="hover ? 'text-circle-hover' : ''"
+                  >
+                    Central do assinante
+                  </span>
+                </div>
+              </v-hover>
+            </div>
+
+            <v-card
+              min-width="420"
+              min-height="420"
+              class="carrousel"
+              elevation="0"
+            >
+              <v-carousel
+                cycle
+                height="420"
+                max-height="420"
+                show-arrows-on-hover
+                hide-delimiter-background
+                hide-delimiters
+                interval="2100"
+              >
+                <v-carousel-item
+                  v-for="(fotos, i) in carrousel"
+                  :key="i"
+                >
+                  <v-img
+                    class="fill-height"
+                    :src="carrousel[i]"
+                    max-height="420"
+                    max-width="420"
+                  >
+                  </v-img>
+                </v-carousel-item>
+              </v-carousel>
+            </v-card>
+          </div>
+
+        </div>
+        <div class="right-center-box">
+          <h2 class="text-central">Central do assinante</h2>
+        </div>
+        
+      </div>
+
+      <div class="bottom-sm">
+        <v-img
+          alt="Linhas da parte de baixo da pagina"
+          src="img/home-baixo.png"
+          class="faixa-bottom"
+        />
+
+        <div>
+          <div class="social-medias d-flex align-center">
+            
             <v-hover
               v-slot="{ hover }"
             >
-              <div
-                class="pl-2 pr-2 pt-1 pb-1 mr-3 ml-3 d-flex flex-column align-center justify-center box"
-                :class="hover ? 'hover-box' : ''"
-              >
-                <div
-                  class="circle d-flex align-center justify-center mb-2"
-                  :class="hover ? 'hover-circle' : ''"
-                >
-                  <v-icon
-                    size="60"
-                    class="icon"
-                    :class="hover ? 'hover-icon' : ''"
-                  >
-                    mdi-book-open-variant
-                  </v-icon>
-                </div>
 
-                <span
-                  class="text-circle"
-                  :class="hover ? 'text-circle-hover' : ''"
+              <v-btn
+                href="https://www.facebook.com/mastertelecom/"
+                target="_blank"
+                color="transparent"
+                elevation="0"
+                width="40"
+              >
+                <v-img
+                  width="45"
+                  height="45"
+                  src="img/icons2/facebook.png"
+                  class="icone-sm"
+                  :class="hover ? 'icone-sm-hover' : ''"
                 >
-                  Sobre a empresa
-                </span>
-              </div>
+                </v-img>
+              </v-btn>
             </v-hover>
 
-            <v-divider vertical></v-divider>
-
             <v-hover
               v-slot="{ hover }"
             >
-              <div
-                class="pl-2 pr-2 pt-1 pb-1 mr-3 ml-3 d-flex flex-column align-center justify-center box"
-                :class="hover ? 'hover-box' : ''"
+
+              <v-btn
+                href="https://www.instagram.com/master.telecom/"
+                target="_blank"
+                color="transparent"
+                elevation="0"
+                width="40"
               >
-                <div
-                  class="circle-orange d-flex align-center justify-center mb-2"
-                  :class="hover ? 'hover-circle' : ''"
+                <v-img
+                  width="35"
+                  height="35"
+                  src="img/icons2/instagram.png"
+                  class="icone-sm"
+                  :class="hover? 'icone-sm-hover' : ''"
                 >
-                  <v-icon
-                    size="60"
-                    class="icon-orange"
-                    :class="hover ? 'hover-icon' : ''"
-                  >
-                    mdi-text-box-check-outline
-                  </v-icon>
-                </div>
-
-                <span
-                  class="text-circle"
-                  :class="hover ? 'text-circle-hover' : ''"
-                >
-                  Conhecer os planos
-                </span>
-              </div>
-            </v-hover>
-
-            <v-divider vertical></v-divider>
-
-            <v-hover
-              v-slot="{ hover }"
-            >
-              <div
-                class="pl-2 pr-2 pt-1 pb-1 mr-3 ml-3 d-flex flex-column align-center justify-center box"
-                :class="hover ? 'hover-box' : ''"
-              >
-                <div
-                  class="circle d-flex align-center justify-center mb-2"
-                  :class="hover ? 'hover-circle' : ''"
-                >
-                  <v-icon
-                    size="60"
-                    class="icon"
-                    :class="hover ? 'hover-icon' : ''"
-                  >
-                    mdi-face-agent
-                  </v-icon>
-                </div>
-
-                <span
-                  class="text-circle text-center"
-                  :class="hover ? 'text-circle-hover' : ''"
-                >
-                  Solicitar Contato
-                </span>
-              </div>
+                </v-img>
+              </v-btn>
             </v-hover>
           </div>
 
-          <v-card
-            min-width="360"
-            min-height="360"
-            class="carrousel"
-            elevation="0"
-          >
-            <v-carousel
-              cycle
-              height="360"
-              max-height="360"
-              show-arrows-on-hover
-              hide-delimiter-background
-              hide-delimiters
-              interval="2100"
-            >
-              <v-carousel-item
-                v-for="(fotos, i) in carrousel"
-                :key="i"
-              >
-                <v-img
-                  class="fill-height"
-                  :src="carrousel[i]"
-                  max-height="360"
-                  max-width="360"
-                >
-                </v-img>
-              </v-carousel-item>
-            </v-carousel>
-          </v-card>
-        </div>
-
-      </div>
-      <div class="right-center-box">
-        <h2 class="text-central">Central do assinante</h2>
-      </div>
-      
-    </div>
-
-    <div class="bottom-sm">
-      <v-img
-        alt="Linhas da parte de baixo da pagina"
-        src="img/home-baixo.png"
-        class="faixa-bottom"
-      />
-
-      <div>
-        <div class="social-medias d-flex align-center">
-          
-          <v-hover
-            v-slot="{ hover }"
-          >
+          <div class="button-whatsapp d-flex">
 
             <v-btn
-              href="https://www.facebook.com/mastertelecom/"
+              href="https://api.whatsapp.com/send?phone=5567992345355&text=Ol%C3%A1!%20Gostaria%20de%20saber%20mais%20sobre%20os%20planos."
               target="_blank"
               color="transparent"
               elevation="0"
               width="40"
             >
               <v-img
-                width="45"
-                height="45"
-                src="img/icons2/facebook.png"
-                class="icone-sm"
-                :class="hover ? 'icone-sm-hover' : ''"
+                width="60"
+                height="60"
+                src="img/icons2/whatsApp.png"
+                class="icon-sm-wpp"
               >
               </v-img>
             </v-btn>
-          </v-hover>
-
-          <v-hover
-            v-slot="{ hover }"
-          >
-
-            <v-btn
-              href="https://www.instagram.com/master.telecom/"
-              target="_blank"
-              color="transparent"
-              elevation="0"
-              width="40"
-            >
-              <v-img
-                width="35"
-                height="35"
-                src="img/icons2/instagram.png"
-                class="icone-sm"
-                :class="hover? 'icone-sm-hover' : ''"
-              >
-              </v-img>
-            </v-btn>
-          </v-hover>
-        </div>
-
-        <div class="button-whatsapp d-flex">
-
-          <v-btn
-            href="https://api.whatsapp.com/send?phone=5567992345355&text=Ol%C3%A1!%20Gostaria%20de%20saber%20mais%20sobre%20os%20planos."
-            target="_blank"
-            color="transparent"
-            elevation="0"
-            width="40"
-          >
-            <v-img
-              width="60"
-              height="60"
-              src="img/icons2/whatsApp.png"
-              class="icon-sm-wpp"
-            >
-            </v-img>
-          </v-btn>
+          </div>
         </div>
       </div>
     </div>
+
+    <v-overlay
+      opacity="1"
+      :value="overlay"
+      color="var(--blue)"
+    >
+
+      <div class="d-flex flex-column align-center justify-center">
+        <v-img
+          max-width="300"
+          src="img/logo.png"
+          class="pulse-logo mb-10"
+        >
+
+
+        </v-img>
+        <v-progress-circular indeterminate size="30" color="var(--orange)">
+        </v-progress-circular>
+        <span class="caption">Carregando...</span>
+      </div>
+
+    </v-overlay>
   </div>
 
 </template>
@@ -251,20 +310,48 @@
     name: 'Home',
     data: ()=>{
       return{
+        overlay: true,
         carrousel: [
           'img/teste-1.jpg',
           'img/teste-2.jpg'
         ]
       }
-    }
+    },
 
+    methods: {
+
+      overlay_home(){
+        setTimeout(()=>{
+          this.overlay = false;
+        }, 4000)
+      },
+
+      teste_link(){
+        alert('botão em construção');
+      },
+
+      redirecionar_link(link){
+        this.overlay = !this.overlay;
+        setInterval(()=>{
+          window.location.href = link;
+        }, 2000)
+      }
+    },
+
+    mounted(){
+      this.overlay_home();
+    }
   }
 </script>
 
 <style scoped>
 
+  .pulse-logo{
+    animation: wpp 2.1s infinite ease-in-out;
+  }
+
   .reduction{
-    width: 80%;
+    width: 100%;
   }
 
   .v-card--reveal{
@@ -295,17 +382,15 @@
   .carrousel{
     border-radius: 8px;
     z-index: 2;
-    padding: 0.3rem;
-    padding-top: 0.7rem;
-    padding-bottom: 0.7rem;
     box-shadow: var(--shadow);
     background: rgba(255, 255, 255,0.75);
+    padding-right: 0;
   }
 
   .circle,
   .circle-orange{
-    width: 80px;
-    height: 80px;
+    width: 60px;
+    height: 60px;
     background: var(--blue);
     border-radius: 50%;
     transition: 0.6s;
@@ -316,17 +401,24 @@
     transition: 0.6s;
   }
 
+  .destaque-hover-circle{
+    background: var(--red);
+    transition: 0.6s;
+    border: solid 1px var(--blue);
+  }
+
   .circle-orange{
     background: var(--orange);
   }
 
   .text-circle{
-    font-size: 1.2rem;
+    font-size: 1.03rem;
     font-weight: 500;
     text-transform: uppercase;
     display: block;
     width: 110px;
     line-height: 24px;
+    margin-top: 10px;
     text-align: center;
     transition: 0.6s;
   }
@@ -336,16 +428,35 @@
     transition: 0.6s;
   }
 
+  .destaque-text-circle{
+    font-weight: 900;
+    color: var(--branco);
+  }
+
   .box{
     transition: 0.6s;
     transform: scale(1);
     border-radius: 4px;
     cursor: pointer;
+    
+    padding: 0.3rem;
+    padding-top: 0.7rem;
+    padding-bottom: 0.7rem;
   }
 
   .hover-box{
     transition: 0.6s;
-    transform: scale(1.15);
+    transform: scale(1.1);
+  }
+
+  .destaque-box{
+    background: var(--orange);
+    transition: 0.6s;
+  }
+
+  .hover-destaque-box{
+    background: var(--blue);
+    transition: 0.6s;
   }
 
   .icon{
@@ -373,13 +484,13 @@
 
   .left-center-box{
     height: 100%;
-    flex: 4;
+    flex: 3;
   }
 
   .right-center-box{
-    display: none;
     background: var(--orange);
     flex: 1;
+    display: none;
   }
 
   .box-carrousel{
@@ -391,9 +502,9 @@
 
   .carrousel{
     padding: 0;
-    margin-left: 30px;
     background: transparent;
     box-shadow: unset;
+    margin-bottom: 90px;
   }
 
   .bottom-sm{
